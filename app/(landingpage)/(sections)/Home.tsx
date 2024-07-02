ac58d21dd36b3d7450960b0d2a13424d87a9e96b
Ralph Saladino <130468937+hplar05@users.noreply.github.com>
2024-07-02 17:06:03 +0800

    fix: responsiveness

--- app/(landingpage)/(sections)/Home.tsx
+++ app/(landingpage)/(sections)/Home.tsx
@@ -9,7 +9,7 @@ export const Home = () => {
   return (
     <section id="home" className="w-full py-12 md:py-24 lg:py-32">
       <div className="container px-4 md:px-6">
-        <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
+        <div className="grid max-md:text-center gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
           <div className="flex flex-col justify-center space-y-4">
             <div className="space-y-2">
               <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl xl:text-5xl/none">
@@ -31,7 +31,7 @@ export const Home = () => {
                 started today and take the first step towards a brighter future.
               </p>
             </div>
-            <div className="flex flex-col gap-2 min-[400px]:flex-row">
+            <div className="flex flex-col max-md:items-center max-md:justify-center gap-2 min-[400px]:flex-row">
               <Link
                 href="/dashboard"
                 className="inline-flex h-10 items-center justify-center rounded-md bg-[#800000] px-8 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-[#990404] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
